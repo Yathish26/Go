eb93bff67fbf19cb9fc9bbc6f29bad547a3b0171
Yathish Acharya <yathishkumar2013@gmail.com>
2024-11-15 15:40:07 +0530

    will it

--- smile.go
+++ smile.go
@@ -5,8 +5,13 @@ import "fmt"
 
 func main(){
 	wilson("Yathish",24)
+	price("Laptop",50000)
 }
 
 func wilson(name string, age int32){
 	fmt.Printf("Hi my name is %v and i am %v years old",name,age)
+}
+
+func price(product string, price int32){
+	fmt.Printf("The price of %v is %v",product,price)
 }
